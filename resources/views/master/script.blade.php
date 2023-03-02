49416d351250546ce72d722e1dc647bb35769e4d
ezazee <yukiirima123@gmail.com>
2023-03-03 02:00:55 +0700

    first commit

---
+++ resources/views/master/script.blade.php
@@ -0,0 +1,51 @@
+<!-- latest jquery-->
+    <script src="{{ URL::asset('assets/js/jquery-3.5.1.min.js') }}"></script>
+    <!-- feather icon js-->
+    <script src="{{ URL::asset('assets/js/icons/feather-icon/feather.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/icons/feather-icon/feather-icon.js') }}"></script>
+    <!-- Sidebar jquery-->
+    <script src="{{ URL::asset('assets/js/sidebar-menu.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/config.js') }}"></script>
+    <!-- Bootstrap js-->
+    <script src="{{ URL::asset('assets/js/bootstrap/popper.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/bootstrap/bootstrap.min.js') }}"></script>
+    <!-- Plugins JS start-->
+    <script src="{{ URL::asset('assets/js/chart/chartist/chartist.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/chartist/chartist-plugin-tooltip.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/knob/knob.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/knob/knob-chart.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/apex-chart/apex-chart.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/apex-chart/stock-prices.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/prism/prism.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/clipboard/clipboard.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/counter/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/counter/jquery.counterup.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/counter/counter-custom.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/custom-card/custom-card.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/notify/bootstrap-notify.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-us-aea-en.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-uk-mill-en.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-au-mill.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-chicago-mill-en.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-in-mill.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vector-map/map/jquery-jvectormap-asia-mill.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/dashboard/default.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/notify/index.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/datepicker/date-picker/datepicker.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/datepicker/date-picker/datepicker.en.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/datepicker/date-picker/datepicker.custom.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/excanvas.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.time.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.categories.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.stack.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.pie.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/jquery.flot.symbol.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/chart/flot-chart/flot-script.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/tooltip-init.js') }}"></script>
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="{{ URL::asset('assets/js/script.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/theme-customizer/customizer.js') }}"></script>
